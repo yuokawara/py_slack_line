663f5941517d48a16eecee4b5bb2c32d5f23898d
yuokawara <solbonta@hotmail.com>
2021-04-09 15:47:23 +0900

    change txt

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
 CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
-WEB_HOOK_LINKS = os.environ["SLACK_WEB_HOOK_URL"]
+WEB_HOOK_LINKS = os.environ["SLACK_WEB_HOOKS_URL"]
 
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
