fcf0618d582cf6409ffbf413cb40eb1667b140dc
yuokawara <solbonta@hotmail.com>
2021-04-14 14:07:55 +0900

    test add

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def callback():
     signature = request.headers['X-Line-Signature']
 
     body = request.get_data(as_text=True)
-    # app.logger.info("test Request" + body)
+    #app.logger.info("test Request" + body)
 
     try:
         handler.handle(body, signature)
