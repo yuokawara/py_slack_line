2408f2f6fab495f76c6a5c8fda8b328401a64e89
yuokawara <solbonta@hotmail.com>
2021-04-15 11:33:29 +0900

    test add

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def callback():
 
     body = request.get_data(as_text=True)
     # app.logger("test Request" + body)
-    print("callback body", body.message)
+    print("!!! callback body !!!", body)
 
     try:
         handler.handle(body, signature)
