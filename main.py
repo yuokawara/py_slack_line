8613bc9350bfa813eb4e2740f7bfed3863483515
yuokawara <solbonta@hotmail.com>
2021-04-16 10:41:40 +0900

    test add

--- main.py
+++ main.py
@@ -135,6 +135,7 @@ def handle_image_message(event):
     res = requests.post(url, params=param, files=files, headers=headers)
     print("res", res.json())
 
+# todo Video
 @handler.add(MessageEvent, message=VideoSendMessage)
 def handle_video_message(event):
 
@@ -150,7 +151,7 @@ def handle_video_message(event):
                + "送信元: {msg_type} ( {room_id} )\n".format(msg_type=msg_type, room_id=room_id) \
                + "送信者: {user_name} ( {user_id} )".format(user_name=user_name, user_id=user_id)
 
-    file_name = "send_image_{message_id}".format(message_id=message_id)
+    file_name = "send_video_{message_id}".format(message_id=message_id)
 
     url = 'https://slack.com/api/files.upload'
     headers = {"Authorization" : "Bearer "+ USER_OAUTH}
